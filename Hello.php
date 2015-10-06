08f4ccf99a0ce74cfbc6d7a9ad4d0173146e766c
Germangalia <germangalia@iesebre.com>
2015-10-06 19:43:59 +0200

    Primera versió

---
+++ Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ggalia84
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+class Hello
+{
+
+    public function sayHello(){
+
+        echo "Hello World!\n";
+    }
+}
---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/Hello.php";
+/**
+ * Created by PhpStorm.
+ * User: ggalia84
+ * Date: 6/10/15
+ * Time: 19:33
+ */
+$greetings = new Hello();
+
+$greetings->sayHello();
